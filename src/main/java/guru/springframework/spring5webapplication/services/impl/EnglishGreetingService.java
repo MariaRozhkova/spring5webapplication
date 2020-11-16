96c3c4e71d459d9bab5895f4a308ee3cca211b12
Rozhkova Mary <mary.wootsen.96@gmail.com>
2020-11-16 20:21:09 +0300

    Add spring default profile

--- src/main/java/guru/springframework/spring5webapplication/services/impl/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring5webapplication/services/impl/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring5webapplication.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service(value = "i18Service")
 public class EnglishGreetingService implements GreetingService {
     @Override

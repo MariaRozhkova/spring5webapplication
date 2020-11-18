b485582d48db587c2ba2da30d78925445d132ac1
Rozhkova Mary <mary.wootsen.96@gmail.com>
2020-11-19 00:47:06 +0300

    Use Spring Environment properties

--- src/main/java/guru/springframework/spring5webapplication/config/PropertyConfig.java
+++ src/main/java/guru/springframework/spring5webapplication/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.spring5webapplication.config;
 
 import guru.springframework.spring5webapplication.datasource.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    private Environment environment;
 
     @Value("${guru.username}")
     private String username;
@@ -21,7 +26,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         final FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(environment.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;

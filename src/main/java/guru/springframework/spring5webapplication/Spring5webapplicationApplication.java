eb721b4de1203b91485d1868704dbf3e56e28c09
Rozhkova Mary <mary.wootsen.96@gmail.com>
2020-11-18 22:19:54 +0300

    Get values from external property file

--- src/main/java/guru/springframework/spring5webapplication/Spring5webapplicationApplication.java
+++ src/main/java/guru/springframework/spring5webapplication/Spring5webapplicationApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapplication;
 
 import guru.springframework.spring5webapplication.controllers.I18nController;
+import guru.springframework.spring5webapplication.datasource.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,6 +14,11 @@ public class Spring5webapplicationApplication {
 
         final I18nController controller = (I18nController) context.getBean("i18nController");
         System.out.println(controller.sayHello());
+
+        final FakeDataSource fakeDataSource = context.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getUrl());
     }
 
 }
